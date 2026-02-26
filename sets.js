284a210ba69632f2ae72af9ef36b8482f9b1d2d6
Nico Tapiero <47359303+nicotapiero@users.noreply.github.com>
2026-02-26 11:26:49 -0500

    Add vocabulary data for Genki 13

--- sets.js
+++ sets.js
@@ -13,5 +13,64 @@ student	学生;がくせい`
 drink	飲む;のむ
 go	行く;いく
 see	見る;みる`
+  },
+  {
+    name: "Genki 13",
+    data: `adult	大人;おとな
+lawyer	弁護士;べんごし
+I (formal)	私;わたくし
+curry	カレー
+black tea	紅茶;こうちゃ
+kimono; Japanese traditional dress	着物;きもの
+sweater	セーター
+musical instrument	楽器;がっき
+karate	空手;からて
+golf	ゴルフ
+motorcycle	バイク
+elephant	象;ぞう
+body	体;からだ
+foreign language	外国語;がいこくご
+language	言葉;ことば
+grammar	文法;ぶんぽう
+application	アプリ
+apartment; smaller apartment building	アパート
+larger apartment building; condominium	マンション
+airport	空港;くうこう
+shop; store	店;みせ
+consumer prices	物価;ぶっか
+advertisement	広告;こうこく
+recruitment	募集;ぼしゅう
+promise; appointment	約束;やくそく
+glad	うれしい
+sad	悲しい;かなしい
+strict	厳しい;きびしい
+to feel sick	気分が悪い;きぶんがわるい
+hot and spicy; salty	辛い;からい
+incredible; awesome	すごい
+close; near	近い;ちかい
+various; different kinds of	いろいろ;いろいろ（な）
+happy (lasting happiness)	しあわせ;しあわせ（な）
+no good	だめ;だめ（な）
+to knit（〜を）	編む;あむ
+to do one's best; to try hard	頑張る;がんばる
+to cry	泣く;なく
+to brush (teeth); to polish（〜を）	磨く;みがく
+to keep a promise	約束を守る;やくそくをまもる
+to be moved/touched (by ...)（〜に）	感動する;かんどうする
+... times	回;かい
+... kilometers; ... kilograms	キロ
+all	全部;ぜんぶ
+my name is ...	と申します;ともうします
+especially	特に;とくに
+one day	一日;いちにち
+two days	二日;ふつか
+three days	三日;みっか
+four days	四日;よっか
+five days	五日;いつか
+six days	六日;むいか
+seven days	七日;なのか
+eight days	八日;ようか
+nine days	九日;ここのか
+ten days	十日;とおか`
   }
 ];
