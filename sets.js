44628e18cfce78dc2df5c84b420960c8bbb13ed6
Nico Tapiero <47359303+nicotapiero@users.noreply.github.com>
2026-02-26 11:19:41 -0500

    Create sets.js

---
+++ sets.js
@@ -0,0 +1,17 @@
+// Predefined study sets
+const PRESET_SETS = [
+  {
+    name: "Basic Japanese Nouns",
+    data: `adult	大人;おとな
+child	子供;こども
+teacher	先生;せんせい
+student	学生;がくせい`
+  },
+  {
+    name: "Basic Verbs",
+    data: `eat	食べる;たべる
+drink	飲む;のむ
+go	行く;いく
+see	見る;みる`
+  }
+];
