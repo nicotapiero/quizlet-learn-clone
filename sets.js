44a45adecadac12bc1aeb62a3d6b293d1c473050
Nico Tapiero <47359303+nicotapiero@users.noreply.github.com>
2026-03-27 20:00:21 -0400

    Add Genki 14 vocabulary data to sets.js

--- sets.js
+++ sets.js
@@ -72,5 +72,60 @@ seven days	七日;なのか
 eight days	八日;ようか
 nine days	九日;ここのか
 ten days	十日;とおか`
+  },
+  {
+    name: "Genki 14",
+    data: `your/someone's wife	奥さん;おくさん
+your/someone's husband	ご主人;ごしゅじん
+partner	パートナー
+uncle; middle-aged man	おじさん
+aunt; middle-aged woman	おばさん
+parents	両親;りょうしん
+landlord; landlady	大家さん;おおやさん
+everyone; all of you	皆さん;みなさん
+chocolate	チョコレート
+mandarin orange	みかん
+sweatshirt	トレーナー
+shirt	シャツ
+necktie	ネクタイ
+winter scarf	マフラー
+ring	指輪;ゆびわ
+pencil	鉛筆;えんぴつ
+stuffed animal (e.g., teddy bear)	ぬいぐるみ
+comic book	漫画;まんが
+cosmetics	化粧品;けしょうひん
+radio	ラジオ
+plate; dish	お皿;おさら
+return (as a token of gratitude)	お返し;おかえし
+résumé	履歴書;りれきしょ
+Christmas	クリスマス
+Valentine's Day	バレンタインデー
+White Day	ホワイトデー
+to want (thing が)	欲しい;ほしい
+fashionable; stylish	おしゃれ;おしゃれ (な)
+stingy; cheap	けち;けち (な)
+to send (person に thing を)	送る;おくる
+to look good (on somebody) (thing が)	似合う;にあう
+to give up	あきらめる
+to give (to others) (person に thing を)	あげる
+to give (me) (person に thing を)	くれる
+to come into existence; to be made	できる
+to consult (person に)	相談する;そうだんする
+to give warning (person に); to watch out (~に)	注意する;ちゅういする
+to propose marriage (person に)	プロポーズする
+Mr./Ms. (casual)	君;くん
+[makes a noun plural]	たち
+we	私たち;わたしたち
+like this; this kind of	こんな
+suddenly	急に;きゅうに
+exactly	ちょうど
+well	よく
+I am not sure...	さあ
+what should one do	どうしたらいい
+generic counter for smaller items	個;こ
+counter for bound volumes	冊;さつ
+counter for equipment	台;だい
+counter for smaller animals	匹;ひき
+counter for long objects	本;ほん`
   }
 ];
